7a07378c4073eabac46e77f7fb038071caf9ce22
QDoa <damilareadeniyi@gmail.com>
2025-04-13 21:50:41 -0600

    Added linkedin link

--- apps/customer/app/page.tsx
+++ apps/customer/app/page.tsx
@@ -10,6 +10,10 @@ const social_media = [
         name: "Instagram",
         link: "https://www.instagram.com/afreglobal?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
     },
+    {
+        name: "Linkedin",
+        link: "https://www.linkedin.com/company/afreglobal/",
+    }
 ]
 
 export default function Home() {
