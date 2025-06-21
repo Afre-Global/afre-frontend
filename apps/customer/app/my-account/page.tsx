9f6132d123b85ec986f278fd10c012fe757fd483
chigoziecele-okafor <celeanthony2@gmail.com>
2025-06-21 06:48:40 -0600

    add my account route and personal info components

---
+++ apps/customer/app/my-account/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import PersonalInfo from "@/app/my-account/components/PersonalInfo";
+
+export default function MyAccountPage() {
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <PersonalInfo />
+    </div>
+  );
+}
