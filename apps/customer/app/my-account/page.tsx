4ec28e718146037f9c601cc70827c41f801b34fb
chigoziecele-okafor <celeanthony2@gmail.com>
2025-06-21 11:21:23 -0600

    implement personal info component

--- apps/customer/app/my-account/page.tsx
+++ apps/customer/app/my-account/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import PersonalInfo from "@/app/my-account/components/PersonalInfo";
+import PersonalInfo, { UserPersonalInfo } from "@/app/my-account/components/PersonalInfo";
+
+const personalInfo: UserPersonalInfo = {
+  name: "john doe",
+  email: "johndoe@gmail.com",
+  phone: "08055830293",
+};
 
 export default function MyAccountPage() {
   return (
-    <div className="bg-gray-50 min-h-screen">
-      <PersonalInfo />
+    <div className="min-h-screen pt-5 px-5">
+      <PersonalInfo personalInfo={personalInfo} />
     </div>
   );
 }
