45493559449510f7d8fb8634ad208e2bdf75633e
QDoa <damilareadeniyi@gmail.com>
2025-06-21 17:09:22 -0600

    Added alerts and moved common variables to shared location

---
+++ packages/shared/src/utils/env.ts
@@ -0,0 +1,5 @@
+export const BUYER_PLATFORM_URL = process.env.NEXT_PUBLIC_BUYER_PLATFORM_URL || "https://www.afreglobal.com/";
+export const BUYER_PLATFORM_URL_CONTACT = BUYER_PLATFORM_URL + "/#contact";
+export const SELLER_PLATFORM_URL = process.env.NEXT_PUBLIC_SELLER_PLATFORM_URL || "https://afreglobalseller.com/"
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://127.0.0.1:8000";
+export const SUBSCRIBE_URL = BACKEND_URL + "/landing_page/subscribe/";
