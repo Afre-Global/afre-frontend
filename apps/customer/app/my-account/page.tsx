df6b92521ac573907daf9563b74de3f01d24a88e
chigoziecele-okafor <celeanthony2@gmail.com>
2025-07-06 00:06:39 -0600

    add notification preferences component

--- apps/customer/app/my-account/page.tsx
+++ apps/customer/app/my-account/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import PersonalInfo, { UserPersonalInfo } from "@/app/my-account/components/PersonalInfo";
+import NotificationPreferences, {
+  UserNotificationPreferences,
+} from "@/app/my-account/components/NotificationPreferences";
 
 const personalInfo: UserPersonalInfo = {
   name: "john doe",
@@ -8,10 +11,16 @@ const personalInfo: UserPersonalInfo = {
   phone: "08055830293",
 };
 
+const notificationPreferences: UserNotificationPreferences = {
+  sms_enabled: true,
+  marketing_enabled: true,
+};
+
 export default function MyAccountPage() {
   return (
     <div className="min-h-screen pt-5 px-5">
       <PersonalInfo personalInfo={personalInfo} />
+      <NotificationPreferences NotificationPreferences={notificationPreferences} />
     </div>
   );
 }
