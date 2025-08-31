63b4ec2d86ac470d21237b0e7aaab29a5d430a1d
chigoziecele-okafor <celeanthony2@gmail.com>
2025-08-30 22:57:45 -0600

    fix push issues

--- apps/customer/app/my-account/components/Order.tsx
+++ apps/customer/app/my-account/components/Order.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import React from "react";
-import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from "@repo/shared/ui";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@repo/shared/ui";
 import { Button } from "@repo/shared/ui";
 import { Badge } from "@repo/shared/ui";
 import { Eye } from "lucide-react";
@@ -22,7 +29,7 @@ export interface OrderItem {
   price: number;
 }
 
-export default function Order({order}: {order: Order}) {
+export default function Order({ order }: { order: Order }) {
   return (
     <>
       <Card>
@@ -31,26 +38,32 @@ export default function Order({order}: {order: Order}) {
           <CardDescription>Placed on {order.placedDate}</CardDescription>
           <CardAction>
             <div className="flex flex-col">
-              <Badge  className="bg-green-100  text-green-800 hover:bg-green-200">{order.status}</Badge>
+              <Badge className="bg-green-100  text-green-800 hover:bg-green-200">
+                {order.status}
+              </Badge>
               <p className="text-sm text-right text-green-800 font-bold">${order.total}</p>
             </div>
           </CardAction>
         </CardHeader>
         <CardContent>
           <div className="flex flex-col gap-2 mb-4">
-          {order.items.map((item) => (
-            <div key={item.id} className="flex flex-row justify-between">
-              <p className="text-sm">{item.name} × {item.quantity}</p>
-              <p className="text-sm">${item.price}</p>
-            </div>
-          ))}
+            {order.items.map((item) => (
+              <div key={item.id} className="flex flex-row justify-between">
+                <p className="text-sm">
+                  {item.name} × {item.quantity}
+                </p>
+                <p className="text-sm">${item.price}</p>
+              </div>
+            ))}
           </div>
           <div className="flex flex-row gap-2">
-              <Button variant="outline"><Eye className="w-4 h-4 mr-3" /> View details</Button>
-              <Button variant="outline">Reorder</Button>
-            </div>
+            <Button variant="outline">
+              <Eye className="w-4 h-4 mr-3" /> View details
+            </Button>
+            <Button variant="outline">Reorder</Button>
+          </div>
         </CardContent>
       </Card>
-    </> 
-  )
+    </>
+  );
 }
